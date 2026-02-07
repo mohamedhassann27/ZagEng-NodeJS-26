cf4e134c6ea2c4bdec34ff9bb27bde9e6bcb372c
mohamed <syab458888543@gmail.com>
2026-02-07 13:57:07 +0200

    update

--- JS/Task-5/script.js
+++ JS/Task-5/script.js
@@ -1,3 +1,4 @@
+
 const studentsList= document.getElementById('students')
 const submitBtn= document.querySelector('button')
 
@@ -22,13 +23,51 @@ submitBtn.addEventListener('click', (e)=>{
         
         const btn= document.createElement('button')
         btn.innerText= 'Delete this Student'
+
+        const hr= document.createElement('hr')
             
-        studentsList.append(liElement,btn)
+        liElement.append(btn, hr)
+        studentsList.append(liElement)
 
         btn.addEventListener('click', (e)=>{
             liElement.remove()
-            btn.remove()
         })
+
     }
     
 })
+
+
+
+
+// const addTodoBtn= document.getElementById('addTodo')
+// const todos= document.getElementById('todos')
+// const todoInput= document.getElementById('todo')
+
+// addTodoBtn.addEventListener('click', (e)=>{
+//     if(todoInput.value){
+//         const newTodoDiv= document.createElement('div')
+    
+//         const todoVal= todoInput.value
+//         const newTodo= document.createElement('p')
+//         newTodo.innerText=todoVal;
+    
+//         const deleteBtn= document.createElement('button')
+//         deleteBtn.innerText='Delete this todo'
+//         deleteBtn.addEventListener('click', ()=>{
+//             newTodoDiv.remove()
+//         })
+    
+//         const colorsInput=document.createElement('input')
+//         colorsInput.setAttribute('type', 'color')
+//         colorsInput.addEventListener('input', (e)=>{
+//             newTodo.style.color= e.target.value
+//         })
+
+//         const br= document.createElement('hr')
+    
+//         newTodoDiv.append(newTodo, colorsInput, deleteBtn, br)
+//         todos.append(newTodoDiv)
+//         todoInput.value=''
+//     }
+// })
